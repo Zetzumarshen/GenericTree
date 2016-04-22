636f63e7f55baa8b6c1ccc8cbd3b460744acac99
dimas <zetzumarshen@gmail.com>
2016-04-22 15:52:23 +0700

    Removed useless try catch block

--- tests/GenericTreeTest.php
+++ tests/GenericTreeTest.php
@@ -9,14 +9,7 @@ class GenericTreeTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals($gt->root->getSizeOfChildren(),0);
 	}
 	
-	public function test_insert() {
-		//$tree = new GenericTree();
-		//$tree->insert(array (), "1");
-		$this->markTestIncomplete();
-	}
-
-	public function tree_provider(){
-		/*
+	public function treeProvider(){
 		$tree = new GenericTree();
 		$tree->insert(array (), "1");
 		$tree->insert(array (), "2");
@@ -26,24 +19,58 @@ class GenericTreeTest extends \PHPUnit_Framework_TestCase {
 		$tree->insert(array (3), "4,2");
 		$tree->insert(array (2), "3,1");
 		$tree->insert(array (2, 0), "3,1,1");
-		return $tree;*/
+		return $tree;
 	}
 	
+	public function test_insert() {
+		$tr = $this->treeProvider();
+		$this->assertEquals("1", $tr->root->getChild(0)->value);
+		$this->assertEquals("4", $tr->root->getChild(3)->value);
+		$this->assertEquals(4, $tr->root->getChild(3)->value);
+		$this->assertEquals("3,1", $tr->root->getChild(2)->getChild(0)->value);
+		$this->assertEquals("3,1,1", $tr->root->getChild(2)->getChild(0)->getChild(0)->value);
+	}
+
 	public function test_inOrder() {
-		/*
+		$tr = $this->treeProvider();
 		$preOp = function () {
-			echo "( ";
+			echo "(";
 		};
 		$inOp = function ($arg) {
 			echo " $arg ";
 		};
 		$postOp = function () {
-			echo " )";
+			echo ")";
 		};
-		$expected_output = "( 1 ( ) 2 ( ) 3 ( 3,1 ( 3,1,1 ( ) ) ) 4 ( 4,1 ( ) 4,2 ( ) ) )";
-		$this->assertEquals($expected_output, $tree($preOp, $inOp, $postOp));
-		*/
-		$this->markTestIncomplete();
+		$expected_output = "( 1 () 2 () 3 ( 3,1 ( 3,1,1 ())) 4 ( 4,1 () 4,2 ()))";
+		$this->expectOutputString($expected_output);
+		$tr->inOrder($preOp, $inOp, $postOp);
+	}
+	
+	public function test_getChild() {
+		$n = $this->treeProvider();
+		$this->assertNotEmpty($n->root->getChild(0));
+		$this->assertNotEmpty($n->root->getChild(1));
+		$this->assertNotEmpty($n->root->getChild(2));
+		$this->assertNotEmpty($n->root->getChild(2)->getChild(0));
+		$this->assertEquals($n->root->getChild(2)->getChild(0)->value,"3,1");
+		$this->assertNotEquals($n->root->getChild(2)->getChild(0)->value,3.1);
+	}
+	
+	/**
+	 * @expectedException Exception
+	 */
+	public function test_exceptionInsert(){
+		$tree = new GenericTree();
+		$tree->insert(array(1), "exc");
+	}
+	
+	/**
+	 * @expectedException Exception
+	 */
+	public function test_exceptionInOrder(){
+		$tree = new GenericTree();
+		$tree->insert(array(1), "exc");
 	}
 }
 
